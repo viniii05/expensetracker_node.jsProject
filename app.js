97b4cd2500898b9ce989f13ee68139929d03b211
viniii05 <vinivt.0520@gmail.com>
2025-02-06 17:27:12 +0530

    Created signup page

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'signup.html'));
+});
+
+app.listen(3000);
